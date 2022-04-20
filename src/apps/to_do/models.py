150fd8e105bb5fc6365daa650bf82c54b4728794
GilsonKedson <gilsonkedson15@outlook.com>
2022-04-20 13:02:33 -0300

    #2 - Inicialização do model app to_do 🚧

--- src/apps/to_do/models.py
+++ src/apps/to_do/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class CardToDo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField('Título do card', max_length=120, blank=False, null=False)
+    created_date = models.DateField('Data de Criação', auto_now_add=True, editable=False)
+    modificated_date = models.DateField('Data de Criação', default=timezone.now, editable=False)
+
+    class Meta:
+        verbose_name = 'Card'
+        verbose_name_plural = 'Cards'
+        
+    def __str__(self):
+        if self.modificated_date:
+            return f"{ self.user.first_name } - {self.modificated_date}"
+        else:
+            return f"{ self.user.first_name } - {self.created_date}"
+        
+
+class Task(models.Model):
+    TODO, IN_PROGRESS, TASK_DONE = 'TTD', 'TIP', 'TD'
+    
+    TASK_STATUS_CHOICES = (
+        (TODO, 'todo'), # Task To Do
+        (IN_PROGRESS, 'in_progress'),  # Task In Progress
+        (TASK_DONE, 'done'), # Task Done
+    )
+    
+    card = models.ForeignKey(CardToDo, on_delete=models.CASCADE)
+    title = models.CharField('Título da tarefa', max_length=120, blank=False, null=False)
+    description = models.TextField('Descrição da tarefa', blank=True)
+    status = models.CharField('Status da tarefa', max_length=3, choices=TASK_STATUS_CHOICES, default=TODO)
+    created_date = models.DateField('Data de Criação', auto_now_add=True, editable=False)
+    modificated_date = models.DateField('Data de Criação', default=timezone.now, editable=False)
+    
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        
+    def __str__(self):
+        return f"{ self.card.user.first_name } - { self.card.title } - {self.title}"
---
+++ src/apps/utils/defaultCards.py
@@ -0,0 +1,11 @@
+FIRST_CARD_DEFAULT = {
+    'title': 'To do',
+}
+
+SECOND_CARD_DEFAULT = {
+    'title': 'In Progress',
+}
+
+THIRD_CARD_DEFAULT = {
+    'title': 'Done',
+}
